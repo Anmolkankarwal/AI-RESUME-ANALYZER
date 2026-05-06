83e8ab38ef9f4d537991147e07c91d639e031a7c
Anmol Kankarwal <anmolkankarwal@gmail.com>
2026-05-06 17:40:26 +0530

    Fixed duplicate import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import PyPDF2
 import docx2txt
 import re
- import os
+
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
